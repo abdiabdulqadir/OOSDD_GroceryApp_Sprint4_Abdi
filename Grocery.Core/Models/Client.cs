fbac32121414833cffe66aa1b2de95142d7a1ab8
abdiabdulqadir <abdi.abdulqadir@windesheim.nl>
2025-10-03 18:24:24 +0200

    vergeten een role toe te voegen en het nu gefixt

--- Grocery.Core/Models/Client.cs
+++ Grocery.Core/Models/Client.cs
@@ -4,6 +4,7 @@
     {
         public string EmailAddress { get; set; }
         public string Password { get; set; }
+        public Role Role { get; set; } = Role.None;
 
         public Client(int id, string name, string emailAddress, string password) : base(id, name)
         {
